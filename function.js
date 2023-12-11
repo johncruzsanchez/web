e87e5fdb26957a8a784c926807ee6b34e207d959
johncruzsanchez <jo.cruzs@duocuc.cl>
2023-12-10 21:18:24 -0300

    Guardando cambios 5

--- function.js
+++ function.js
@@ -3,39 +3,42 @@ function mostrarContenidoSegunDispositivo() {
     // Verificar si el dispositivo es un móvil
     var esMovil = (typeof window.orientation !== 'undefined') || (navigator.userAgent.indexOf('IEMobile') !== -1);
 
-    // Crear un contenedor para el contenido
-    var contenedor = document.getElementById('contenidoDescarga');
-
-    // Limpiar el contenido existente en el contenedor
-    contenedor.innerHTML = '';
-
-    // Si es un móvil, mostrar un diseño específico para móviles; de lo contrario, mostrar otro diseño
+    // Si es un móvil, mostrar el botón de descarga APK; de lo contrario, mostrar la imagen QR
     if (esMovil) {
-        mostrarContenidoParaMovil(contenedor);  // Mostrar contenido específico para móviles
+        mostrarContenidoMovil();  // Cambiado a mostrarContenidoMovil para móviles
     } else {
-        mostrarContenidoParaPC(contenedor);  // Mostrar contenido específico para PCs
+        mostrarContenidoPC();  // Cambiado a mostrarContenidoPC para PCs
     }
 }
 
-// Función para mostrar contenido específico para móviles
-function mostrarContenidoParaMovil(contenedor) {
-    // Crear un botón de descarga para móviles
+// Función para mostrar el contenido en dispositivos móviles
+function mostrarContenidoMovil() {
+    // Crear un botón de descarga con un estilo diferente
     var botonDescarga = document.createElement('a');
-    botonDescarga.href = 'https://github.com/';
-    botonDescarga.innerHTML = 'Descargar la aplicación para móviles';
+    botonDescarga.href = 'https://github.com/johncruzsanchez/apk/releases/download/v1.0.0/RegistsAPP.apk';
+    botonDescarga.innerHTML = 'Obtener la App';
     botonDescarga.setAttribute('download', 'RegistrAPP.apk');
+    botonDescarga.classList.add('boton-movil'); // Agregado un estilo específico para móviles
 
-    // Agregar el botón al contenedor
-    contenedor.appendChild(botonDescarga);
+    // Agregar el botón al cuerpo del documento
+    document.body.appendChild(botonDescarga);
 }
 
-// Función para mostrar contenido específico para PCs
-function mostrarContenidoParaPC(contenedor) {
-    // Crear una imagen QR para PCs
-    var imagenQR = document.createElement('img');
-    imagenQR.src = 'imagen-qr.png';
-    imagenQR.alt = 'Código QR para la descarga en PC';
+// Función para mostrar el contenido en PC
+function mostrarContenidoPC() {
+    // Obtener el elemento con el ID 'contenidoDescarga'
+    var contenidoDescarga = document.getElementById('contenidoDescarga');
 
-    // Agregar la imagen al contenedor
-    contenedor.appendChild(imagenQR);
+    // Crear una imagen QR con un estilo diferente
+    var imagenQR = document.createElement('img');
+    imagenQR.src = 'qr.png';
+    imagenQR.alt = 'Código QR para la descarga de la app';
+    imagenQR.classList.add('imagen-qr-pc'); // Agregado un estilo específico para PCs
+
+    // Agregar la imagen al contenido de descarga o al cuerpo del documento si no hay elemento con ID 'contenidoDescarga'
+    if (contenidoDescarga) {
+        contenidoDescarga.appendChild(imagenQR);
+    } else {
+        document.body.appendChild(imagenQR);
+    }
 }
